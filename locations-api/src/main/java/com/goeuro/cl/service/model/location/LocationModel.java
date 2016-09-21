89ec1b17d40958bfe123797d14f70037bfa2c4c5
alexc <alexc@Lappy>
2016-09-21 09:07:27 +0300

    Model added

---
+++ locations-api/src/main/java/com/goeuro/cl/service/model/location/LocationModel.java
@@ -0,0 +1,52 @@
+package com.goeuro.cl.service.model.location;
+
+import javax.xml.bind.annotation.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@XmlRootElement
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class LocationModel {
+
+    @XmlElement(name = "_id")
+    private Long id;
+    @XmlElement
+    private String name;
+    @XmlElement
+    private String type;
+
+    private GeoPosition geoPosition;
+
+    public GeoPosition getGeoPosition() {
+        return geoPosition;
+    }
+
+    public void setGeoPosition(GeoPosition geoPosition) {
+        this.geoPosition = geoPosition;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
